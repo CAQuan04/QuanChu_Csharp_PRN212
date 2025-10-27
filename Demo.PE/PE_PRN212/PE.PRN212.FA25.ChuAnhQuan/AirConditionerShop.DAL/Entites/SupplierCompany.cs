ded777f74aef5f3d19d47be8d98710b1dae74871
QuanChu <quanchu505523@gmail.com>
2025-10-27 11:58:19 +0700

    DEMO.PE.FA25

---
+++ Demo.PE/PE_PRN212/PE.PRN212.FA25.ChuAnhQuan/AirConditionerShop.DAL/Entites/SupplierCompany.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirConditionerShop.DAL.Entites;
+
+public partial class SupplierCompany
+{
+    public string SupplierId { get; set; } = null!;
+
+    public string SupplierName { get; set; } = null!;
+
+    public string? SupplierDescription { get; set; }
+
+    public string? PlaceOfOrigin { get; set; }
+
+    public virtual ICollection<AirConditioner> AirConditioners { get; set; } = new List<AirConditioner>();
+}
