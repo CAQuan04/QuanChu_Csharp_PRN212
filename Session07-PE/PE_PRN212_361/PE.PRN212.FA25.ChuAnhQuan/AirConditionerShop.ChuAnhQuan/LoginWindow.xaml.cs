5bda4cb17d6e9d0b144734abbfb6b0e71448c61b
QuanChu <quanchu505523@gmail.com>
2025-10-27 11:46:20 +0700

    Practice.PE.PRN212

---
+++ Session07-PE/PE_PRN212_361/PE.PRN212.FA25.ChuAnhQuan/AirConditionerShop.ChuAnhQuan/LoginWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AirConditionerShop.ChuAnhQuan
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new(); 
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult answer = MessageBox.Show("Are you sure ?", "quit", MessageBoxButton.YesNo);
+            if (answer == MessageBoxResult.Yes)
+            {
+                //tắt app
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
